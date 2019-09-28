78b84ab92ce9372673b8bcbee759142d46d64ca7
jomajira2511 <54144793+jomajira2511@users.noreply.github.com>
2019-09-28 10:36:37 -0500

    Add files via upload

---
+++ programa que sume dos numeros a=7 b=4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	int a=7, b=4,suma;
+	suma=a+b;
+	cout<<"la suma es igual a "<<suma;
+	
+	return 0;
+}
