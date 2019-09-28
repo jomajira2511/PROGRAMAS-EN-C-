d52cbdadf60b1e65d9e16f3e1dba32a54ab5a7d3
jomajira2511 <54144793+jomajira2511@users.noreply.github.com>
2019-09-28 10:50:30 -0500

    Add files via upload

---
+++ 4.Programa que sume reste multiplique y divida x=10 y=2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	int x=10, y=2,suma,resta,multiplicacion,division;
+	suma=x+y;
+	cout<<"la suma es igual a "<<suma<<endl;
+	resta=x-y;
+	cout<<"la resta es igual a "<<resta<<endl;
+	multiplicacion=x*y;
+	cout<<"la multiplicacion es igual a "<<multiplicacion<<endl;
+	division=x/y;
+	cout<<"la division es igual a "<<division<<endl;
+	
+	return 0;
+}
