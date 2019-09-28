01ea904188773bc526fb35fe078318509d8770c6
jomajira2511 <54144793+jomajira2511@users.noreply.github.com>
2019-09-28 11:04:40 -0500

    Add files via upload

---
+++ 6.Programa que calcule el area de un rectangulo lado1=3 lado2=4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	int l1=3,l2=4,suma;
+	suma=l1*l2;
+	cout<<"el area de un rectangulo es igual a "<<suma;
+	return 0;
+}
