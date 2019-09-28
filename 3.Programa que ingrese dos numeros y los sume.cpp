6b41eeba6909660520baa30fefbc9e1d9d09137a
jomajira2511 <54144793+jomajira2511@users.noreply.github.com>
2019-09-28 10:43:04 -0500

    Add files via upload

---
+++ 3.Programa que ingrese dos numeros y los sume.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main() {
+	int n1,n2,suma;
+	cout<<"ingrese un numero"<<endl;cin>>n1;
+	cout<<"ingrese otro numero"<<endl;cin>>n2;
+	suma=n1+n2;
+	cout<<"la suma es igual a "<<suma;
+	return 0;
+}
