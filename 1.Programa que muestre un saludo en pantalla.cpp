4ec07d9c3e66849ab891d90ca7435f193b76927d
jomajira2511 <54144793+jomajira2511@users.noreply.github.com>
2019-09-30 22:17:03 -0500

    Update 1.Programa que muestre un saludo en pantalla.cpp

--- 1.Programa que muestre un saludo en pantalla.cpp
+++ 1.Programa que muestre un saludo en pantalla.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 using namespace std;
 int main() {
 	cout<<"hola mundo"<<endl;
